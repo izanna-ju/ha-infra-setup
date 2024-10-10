efc95350bac34fec69c8753c5856eb17fca01663
Julius Izanna <izanna.rukky@gmail.com>
2024-10-10 17:51:54 +0100

    Add Infrastructure components to terraform configurations

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "web_bucket" {
-  bucket_prefix = "izanna-web-bucket"
+  bucket = "izanna-web-bucket"
   force_destroy = true
 }
 
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "instance_type" {
+  description = "The instance type"
+  type        = string
+  default     = "t3.medium"
+}
