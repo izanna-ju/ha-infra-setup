c47f0568149a71f822d960635a2e4e5cd4b40af5
Julius Izanna <izanna.rukky@gmail.com>
2024-10-14 07:45:42 +0100

    output: get load balancer dns name

--- web_script.sh
+++ web_script.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
+set -e
+set -x
 
-apt-get update -y
+# Update and install prerequisites
+apt update -y
+apt upgrade -y
+apt install unzip -y
+
+# Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+# Install Apache
 apt-get install apache2 -y                    
 systemctl start apache2.service
+
+# Copy file from S3 bucket
 cd /var/www/html
 aws s3 cp s3://izanna-web-bucket/sample_index.html .
-systemctl restart apache2.service    
+
+cp sample_index.html index.html
+
+# Restart Apache
+systemctl restart apache2.service
