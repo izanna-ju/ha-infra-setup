4aefecd4c66b66e8a8f4c9ace5b6c8a580a0714f
Julius Izanna <izanna.rukky@gmail.com>
2024-11-05 02:24:38 +0100

    Set up backend state

---
+++ remote_backend/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  backend "s3" {
+    key = "infra/import-bootstrap/terraform.tfstate"  
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "iamadmin-general"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "izanna-infra-tf-state"
+  force_destroy = true
+
+  tags = {
+    Name        = "state-bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_bucket_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "state_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
